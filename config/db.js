cfc0c4b057a6d3d591a89c66d79c4d9066a1f861
vaishalitech01 <vaishalitech.info@gmail.com>
2025-10-02 17:13:36 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URL from env:", process.env.MONGO_URL); // debugging
     await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB connected");
   } catch (error) {
