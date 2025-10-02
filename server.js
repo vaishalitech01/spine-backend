c819c55cecf5ea151b9d59e64d75db08e501b209
vaishalitech01 <vaishalitech.info@gmail.com>
2025-10-02 14:36:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ import upiVerificationRoutes from './routes/upiVerificationRoutes.js';
 import addressVerify from "./routes/addressRoute.js";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
